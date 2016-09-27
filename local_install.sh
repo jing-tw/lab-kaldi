7b6942b8533a61dd0ed010739bcf684bd430ea2a
jing-tw <mqjing@gmail.com>
2016-09-27 04:15:14 -0700

    fix build script in folder issue

--- local_install.sh
+++ local_install.sh
@@ -4,11 +4,16 @@ git clone https://github.com/kaldi-asr/kaldi.git kaldi --origin upstream
 sudo apt-get -y install  g++ zlib1g-dev automake autoconf libtool subversion libatlas3-base
 sudo ln -s -f bash /bin/sh
 
+curdir=`pwd`
+
 echo "Build ..."
-cd ~/kaldi/tools/extras/
+cd $curdir/kaldi/tools/extras/
 ./check_dependencies.sh
 
+cd $curdir/kaldi/tools
 make
 
-cd ~/kaldi/src/
+cd $curdir/kaldi/src/
 ./configure --shared
+
+make all
